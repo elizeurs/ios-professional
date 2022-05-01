35ad244561f7942c35fcdfb63160d42a8d298196
Elizeu RS <13ders@gmail.com>
2022-05-01 13:57:53 -0300

    feat: add tableview header

--- Bankey/Bankey/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummaryViewController.swift
@@ -8,73 +8,67 @@
 import UIKit
 
 class AccountSummaryViewController: UIViewController {
-    
-    let games = [
-        "Pacman",
-        "Space Invaders",
-        "Space Patrol",
-    ]
-    
-    var tableView = UITableView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setup()
-    }
+  
+  let games = [
+    "Pacman",
+    "Space Invaders",
+    "Space Patrol",
+  ]
+  
+  var tableView = UITableView()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setup()
+  }
 }
 
 extension AccountSummaryViewController {
-    private func setup() {
-        setupTableView()
-<<<<<<< HEAD
-        setupTableHeaderView()
-=======
->>>>>>> dcae4c1f3da33d6b87d80d568326ed3465194bdd
-    }
+  private func setup() {
+    setupTableView()
+    setupTableHeaderView()
+  }
+  
+  private func setupTableView() {
+    tableView.delegate = self
+    tableView.dataSource = self
     
-    private func setupTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(tableView)
-        
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-    }
-<<<<<<< HEAD
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(tableView)
+    
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+    ])
+  }
   
   private func setupTableHeaderView() {
     let header = AccountSummaryHeaderView(frame: .zero)
     
     var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
-    size.width = UIScreen.main.bounds.width
+    size.width  = UIScreen.main.bounds.width
     header.frame.size = size
     
     tableView.tableHeaderView = header
   }
-=======
->>>>>>> dcae4c1f3da33d6b87d80d568326ed3465194bdd
 }
 
 extension AccountSummaryViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = games[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return games.count
-    }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = games[indexPath.row]
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return games.count
+  }
 }
 
 extension AccountSummaryViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+  }
 }
